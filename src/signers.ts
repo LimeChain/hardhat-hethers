e3ab0cc15a2dd4a3c5cb36c712903aea598fbce2
Ivo Yankov <ivo@devlabs.bg>
2022-02-25 13:16:49 +0200

    fix: provide a working build procedure

--- src/signers.ts
+++ src/signers.ts
@@ -1,49 +1,49 @@
-import { hethers } from "hethers";
-
-export class SignerWithAddress extends hethers.Signer {
-  public static async create(signer: hethers.providers.JsonRpcSigner) {
-    return new SignerWithAddress(await signer.getAddress(), signer);
-  }
-
-  private constructor(
-    public readonly address: string,
-    private readonly _signer: hethers.providers.JsonRpcSigner
-  ) {
-    super();
-    (this as any).provider = _signer.provider;
-  }
-
-  public async getAddress(): Promise<string> {
-    return this.address;
-  }
-
-  public signMessage(message: string | hethers.utils.Bytes): Promise<string> {
-    return this._signer.signMessage(message);
-  }
-
-  public signTransaction(
-    transaction: hethers.utils.Deferrable<hethers.providers.TransactionRequest>
-  ): Promise<string> {
-    return this._signer.signTransaction(transaction);
-  }
-
-  public sendTransaction(
-    transaction: hethers.utils.Deferrable<hethers.providers.TransactionRequest>
-  ): Promise<hethers.providers.TransactionResponse> {
-    return this._signer.sendTransaction(transaction);
-  }
-
-  public connect(provider: hethers.providers.Provider): SignerWithAddress {
-    return new SignerWithAddress(this.address, this._signer.connect(provider));
-  }
-
-  public _signTypedData(
-    ...params: Parameters<hethers.providers.JsonRpcSigner["_signTypedData"]>
-  ): Promise<string> {
-    return this._signer._signTypedData(...params);
-  }
-
-  public toJSON() {
-    return `<SignerWithAddress ${this.address}>`;
-  }
-}
+// import { hethers } from "@hashgraph/hethers";
+//
+// export class SignerWithAddress extends hethers.Signer {
+//   public static async create(signer: hethers.providers.JsonRpcSigner) {
+//     return new SignerWithAddress(await signer.getAddress(), signer);
+//   }
+//
+//   private constructor(
+//     public readonly address: string,
+//     private readonly _signer: hethers.providers.JsonRpcSigner
+//   ) {
+//     super();
+//     (this as any).provider = _signer.provider;
+//   }
+//
+//   public async getAddress(): Promise<string> {
+//     return this.address;
+//   }
+//
+//   public signMessage(message: string | hethers.utils.Bytes): Promise<string> {
+//     return this._signer.signMessage(message);
+//   }
+//
+//   public signTransaction(
+//     transaction: hethers.utils.Deferrable<hethers.providers.TransactionRequest>
+//   ): Promise<string> {
+//     return this._signer.signTransaction(transaction);
+//   }
+//
+//   public sendTransaction(
+//     transaction: hethers.utils.Deferrable<hethers.providers.TransactionRequest>
+//   ): Promise<hethers.providers.TransactionResponse> {
+//     return this._signer.sendTransaction(transaction);
+//   }
+//
+//   public connect(provider: hethers.providers.Provider): SignerWithAddress {
+//     return new SignerWithAddress(this.address, this._signer.connect(provider));
+//   }
+//
+//   public _signTypedData(
+//     ...params: Parameters<hethers.providers.JsonRpcSigner["_signTypedData"]>
+//   ): Promise<string> {
+//     return this._signer._signTypedData(...params);
+//   }
+//
+//   public toJSON() {
+//     return `<SignerWithAddress ${this.address}>`;
+//   }
+// }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,45 +1,45 @@
-import type * as ethers from "ethers";
-import { Artifact } from "hardhat/types";
+import type * as hethers from "@hashgraph/hethers";
 
-import type { SignerWithAddress } from "../signers";
+// import { Artifact } from "hardhat/types";
+// import type { SignerWithAddress } from "../signers";
 
 export interface Libraries {
   [libraryName: string]: string;
 }
 
 export interface FactoryOptions {
-  signer?: ethers.Signer;
+  signer?: hethers.Signer;
   libraries?: Libraries;
 }
 
 export declare function getContractFactory(
   name: string,
-  signerOrOptions?: ethers.Signer | FactoryOptions
-): Promise<ethers.ContractFactory>;
+  signerOrOptions?: hethers.Signer | FactoryOptions
+): Promise<hethers.ContractFactory>;
 export declare function getContractFactory(
   abi: any[],
-  bytecode: ethers.utils.BytesLike,
-  signer?: ethers.Signer
-): Promise<ethers.ContractFactory>;
+  bytecode: hethers.utils.BytesLike,
+  signer?: hethers.Signer
+): Promise<hethers.ContractFactory>;
 
 export interface HardhatEthersHelpers {
-  provider: ethers.providers.JsonRpcProvider;
+  provider: hethers.providers.DefaultHederaProvider;
 
-  getContractFactory: typeof getContractFactory;
-  getContractFactoryFromArtifact: (
-    artifact: Artifact,
-    signerOrOptions?: ethers.Signer | FactoryOptions
-  ) => Promise<ethers.ContractFactory>;
-  getContractAt: (
-    nameOrAbi: string | any[],
-    address: string,
-    signer?: ethers.Signer
-  ) => Promise<ethers.Contract>;
-  getContractAtFromArtifact: (
-    artifact: Artifact,
-    address: string,
-    signer?: ethers.Signer
-  ) => Promise<ethers.Contract>;
-  getSigner: (address: string) => Promise<SignerWithAddress>;
-  getSigners: () => Promise<SignerWithAddress[]>;
+  // getContractFactory: typeof getContractFactory;
+  // getContractFactoryFromArtifact: (
+  //   artifact: Artifact,
+  //   signerOrOptions?: ethers.Signer | FactoryOptions
+  // ) => Promise<ethers.ContractFactory>;
+  // getContractAt: (
+  //   nameOrAbi: string | any[],
+  //   address: string,
+  //   signer?: ethers.Signer
+  // ) => Promise<ethers.Contract>;
+  // getContractAtFromArtifact: (
+  //   artifact: Artifact,
+  //   address: string,
+  //   signer?: ethers.Signer
+  // ) => Promise<ethers.Contract>;
+  // getSigner: (address: string) => Promise<SignerWithAddress>;
+  // getSigners: () => Promise<SignerWithAddress[]>;
 }
